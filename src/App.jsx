0eb64eb5476ed6ac6cac10e873a7c869f4d7cf88
Yuvraj Singh <yuvrajsingh1704@gmail.com>
2022-10-09 16:22:02 +0530

    add-github-link

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import yt_icon from './assets/youtube-icon.svg'
 import linkedin_icon from './assets/linkedin-icon.svg'
 import insta_icon from './assets/instagram-icon.svg'
 import blue_icon from './assets/bluelearn-icon.svg'
+import github_icon from './assets/icons8-github-96.svg'
 
 function App() {
   const [tasks, setTasks] = useLocalStorage('react-todo.tasks', []);
@@ -66,6 +67,7 @@ function App() {
       <li > <a href="https://www.instagram.com/bluelearn.in/"> <img id="logos" src= {insta_icon} ></img></a></li>
       <li > <a href="https://www.linkedin.com/company/bluelearn"><img id="logos" src= {linkedin_icon} ></img></a></li>
       <li > <a href="https://www.youtube.com/channel/UCSuCYJ_jvzVJYFycR4WIZhw"><img id="logos" src={yt_icon}  ></img></a></li>
+      <li > <a href="https://www.youtube.com/channel/UCSuCYJ_jvzVJYFycR4WIZhw"><img id="logos" src={github_icon}></img></a></li>
 
         </ul>
         </footer>
